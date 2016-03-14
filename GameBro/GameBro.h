dffee5dab31f42b111195dad4c5a457301a2b5de
rpowelll <rhys@rpowell.me>
2016-03-14 19:22:16 +1100

    Initial Commit

---
+++ GameBro/GameBro.h
@@ -0,0 +1,19 @@
+//
+//  GameBro.h
+//  GameBro
+//
+//  Created by Rhys Powell on 14/03/2016.
+//  Copyright © 2016 Rhys Powell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GameBro.
+FOUNDATION_EXPORT double GameBroVersionNumber;
+
+//! Project version string for GameBro.
+FOUNDATION_EXPORT const unsigned char GameBroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameBro/PublicHeader.h>
+
+
