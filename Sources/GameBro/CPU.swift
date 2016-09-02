d56a5bc28a3dfa5fbad149ff3a054ed08e1cd8fe
Rhys Powell <rhys@rpowell.me>
2016-09-02 13:22:29 +1000

    Better docs

--- Sources/GameBro/CPU.swift
+++ Sources/GameBro/CPU.swift
@@ -24,7 +24,6 @@
 /// + N - Subtract - Set if a subtraction was performed in the last math operation
 /// + H - Half-Carry - Set if a carry occurred from the lower nibble in the last math operation
 /// + C - Carry - Set if a carry occurred in the last math operation, or if the accumulator A is less than value for a CP operation
-
 public struct CPU {
 
     /// The stack pointer
@@ -57,10 +56,17 @@ public struct CPU {
     /// The L register
     public var L: UInt8 = 0
 
+    /// The CPU's memory
     public var memory: Memory
 
+    /// The number of elapsed cycles
     public var cycle: UInt64 = 0
 
+    /// Create a new CPU with the provided memory
+    ///
+    /// - parameter memory: the memory this CPU will use
+    ///
+    /// - returns: a CPU with the provided memory
     public init(memory: Memory) {
         self.memory = memory
     }
@@ -69,6 +75,8 @@ public struct CPU {
 
 // Pseudo 16-bit registers
 public extension CPU {
+    /// The AF register
+    /// A 16-bit register made from the 8-bit A and F registers
     public var AF: UInt16 {
         get {
             return UInt16(A) << 8 | UInt16(F)
@@ -79,6 +87,8 @@ public extension CPU {
         }
     }
 
+    /// The BC register
+    /// A 16-bit register made from the 8-bit B and C registers
     public var BC: UInt16 {
         get {
             return UInt16(B) << 8 | UInt16(C)
@@ -88,7 +98,8 @@ public extension CPU {
             C = UInt8(newValue & 0x00FF)
         }
     }
-
+    /// The DE register
+    /// A 16-bit register made from the 8-bit D and E registers
     public var DE: UInt16 {
         get {
             return UInt16(D) << 8 | UInt16(E)
@@ -98,7 +109,8 @@ public extension CPU {
             E = UInt8(newValue & 0x00FF)
         }
     }
-
+    /// The HL register
+    /// A 16-bit register made from the 8-bit H and L registers
     public var HL: UInt16 {
         get {
             return UInt16(H) << 8 | UInt16(L)
@@ -113,11 +125,12 @@ public extension CPU {
 // Flags
 public extension CPU {
 
-    internal func getFlag(_ flag: UInt8) -> Bool {
+
+    private func getFlag(_ flag: UInt8) -> Bool {
         return (flag & F) != 0
     }
 
-    internal mutating func setFlag(_ flag: UInt8, _ value: Bool) {
+    private mutating func setFlag(_ flag: UInt8, _ value: Bool) {
         if value {
             F |= flag
         } else {
